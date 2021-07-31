a53f6d2941acd61dced79e004e7790082846a666
Khoa Nguyen <khoa.nguyen@pops.vn>
2021-07-31 20:33:54 +0700

    Finish 7 test cases in flightSurety.js

--- src/server/server.js
+++ src/server/server.js
@@ -9,7 +9,6 @@ let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('htt
 web3.eth.defaultAccount = web3.eth.accounts[0];
 let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
 
-
 flightSuretyApp.events.OracleRequest({
     fromBlock: 0
   }, function (error, event) {
